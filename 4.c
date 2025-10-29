b97aa8cc2bfb6284edb42d60f81e005ec3a654b6
Dnyaneshwar Chitale <dnchitale05@gmail.com>
2025-10-29 14:46:16 +0530

    Remove main function and related code

--- 4.c
+++ 4.c
@@ -1,28 +1,7 @@
-# include <stdio.h>
 
-
-   int main   ()
-   {
-     int e,b,m,p,c;
-
-         printf("Enter the marc of english: ");
-          scanf(" %d",&e);
-           printf("Enter the marc of biology: ");
-          scanf(" %d",&b);
-          printf("Enter the marc of math: ");
-          scanf(" %d",&m);
-          printf("Enter the marc of physics: ");
-          scanf(" %d",&p);
-          printf("Enter the marc of chemistry: ");
-          scanf(" %d",&c);
-      float sum=e+b+m+p+c;
-       float percentage=sum/5;  
-        // printf("e+ b + m+ p+ c")/5;
-          printf("percentage=%f %% ", percentage);
-          return 0;
-   }
     
 
 
 
    
+
